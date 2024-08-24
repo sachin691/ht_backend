b3859667d5522dc4869925967a97ad1da83fe190
sachin <sachinkaurav65@gmail.com>
2024-08-24 21:01:18 +0530

    setup project

---
+++ app.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+// const passport = require("passport");
+const bodyParser = require("body-parser");
+
+
+const port = process.env.PORT;
+const app = express();
+
+
+// Essential Middlewares
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+// app.use(passport.initialize());
+app.use(cors());
+
+
+app.listen(port, () => {
+  console.log("Server Listening on PORT:", port);
+});
